1b7331f3b2f051cf4cbdae291bb28c745f26bb1d
carlos <carl.devfs@gmail.com>
2022-09-19 17:09:06 +0200

    use highlight to parse code blocks

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import './App.scss';
 import Preview from "./components/Preview/Preview";
-import {ChangeEvent, useState} from "react";
+import {useState} from "react";
 import {marked} from 'marked';
 import Editor from "./components/Editor/Editor";
+import hljs from "highlight.js"
 
 function App() {
     const [areaValue, setAreaValue] = useState<string>("")
 
-    const handleOutput = (e: ChangeEvent<HTMLTextAreaElement>) => {
-        setAreaValue(() => marked.parse(e.target.value))
+
+    const handleOutput = (areaValue: string) => {
+        marked.setOptions({
+            breaks: true,
+            gfm: true,
+            highlight: function (code, lang) {
+                const language = hljs.getLanguage(lang) ? lang : 'javascript';
+                return hljs.highlight(code, {language}).value;
+            },
+        })
+        setAreaValue(() => marked.parse(areaValue))
     }
 
     return (
